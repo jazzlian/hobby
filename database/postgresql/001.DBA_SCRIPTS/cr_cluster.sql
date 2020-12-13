1df947d84602f6423161f358dce3661932df5f9a
MyoungSigYoun <jazzlian@gmail.com>
2020-12-13 21:30:50 +0900

    2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file 2020.12.13 add file

--- database/postgresql/001.DBA_SCRIPTS/cr_cluster.sql
+++ database/postgresql/001.DBA_SCRIPTS/cr_cluster.sql
@@ -1,12 +1,16 @@
 #
 initdb –D $PGDATA  \
-      -–lc-collate=“C” \
-      --lc-ctype=“C”  \
-      --encoding=UTF8 \
-      --auth=scram-sha-256 --auth-host=scram-sha-256 --auth-local=scram-sha-256 \
+      -–no-locale \
+      --encoding=UTF-8 \
+      --auth=scram-sha-256 --auth=peer --auth-host=scram-sha-256 --auth-local=scram-sha-256 \
       --data-checksums --pwprompt --username=postgres \
       --waldir=$WALDIR --wal-segsize=128 \
 
 #  enterprisedb option      
 # --redwood-like       
 # --no-redwood-compat (std)
+
+#
+sed -i ‘s#/var/lib/edb/as12/data#/ pgdata/data#g’ /usr/lib/systemd/ system/edb-as-12.service
+systemctl enable edb-as-12
+systemctl start edb-as-12
