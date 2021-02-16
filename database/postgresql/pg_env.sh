b67a2a49671e3c92c9180a5cbfc75e6d0498cbd0
MyoungSigYoun <msyoun@ms-msyoun-01.rockplace.co.kr>
2021-02-16 17:46:56 +0900

    2021.02.16 add file

--- database/postgresql/pg_env.sh
+++ database/postgresql/pg_env.sh
@@ -4,9 +4,11 @@ export	PGPORT=5432
 export	PGUSER=postgres
 export	PGDATA=/data/PG13
 export	PGDATABASE=postgres
-export  DATA_SOURCE_NAME="postgresql://postgres:audtlr2@localhost:5432/postgres?sslmode=disable"
+export  PGSSLMODE=require
+export  PGAPPNAME="MSYOUN-PSQL"
 #
 export  PATH=$PGHOME/bin:$PATH
 export  LD_LIBRARY_PATH=$PGHOME/lib:/lib64:/usr/lib64:.
+export  MANPATH=$PGHOME/share/man:$MANPATH
 #
 export  EDITOR=vim
